0305bc0a8718a6c067e58ecdbdf57ca1eedb727a
Ramesh Sambu <rameshsambu44@gmail.com>
2018-01-11 16:46:20 +0530

    Initial Commit
    Initial Commit

---
+++ app/src/main/java/com/paytmexample/MainActivity.java
@@ -0,0 +1,89 @@
+package com.paytmexample;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+import com.paytm.pgsdk.Log;
+import com.paytm.pgsdk.PaytmOrder;
+import com.paytm.pgsdk.PaytmPGService;
+import com.paytm.pgsdk.PaytmPaymentTransactionCallback;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        onStartTransaction();
+    }
+
+    public void onStartTransaction() {
+        PaytmPGService Service = PaytmPGService.getStagingService();
+        Map<String, String> paramMap = new HashMap<String, String>();
+        paramMap.put("MID" , "WorldP64425807474247");      //Your Merchant ID
+        paramMap.put("ORDER_ID" , "TestMerchant000111007");
+        paramMap.put("CUST_ID" , "mohit.aggarwal@paytm.com");
+        paramMap.put("INDUSTRY_TYPE_ID" , "Retail");
+        paramMap.put("CHANNEL_ID" , "WAP");
+        paramMap.put("TXN_AMOUNT" , "1");
+        paramMap.put("WEBSITE" , "worldpressplg");
+        paramMap.put("CALLBACK_URL" , "https://pguat.paytm.com/paytmchecksum/paytmCallback.jsp");
+        paramMap.put("CHECKSUMHASH" , "w2QDRMgp1/BNdEnJEAPCIOmNgQvsi+BhpqijfM9KvFfRiPmGSt3Ddzw+oTaGCLneJwxFFq5mqTMwJXdQE2EzK4px2xruDqKZjHupz9yXev4=");
+        PaytmOrder Order = new PaytmOrder(paramMap);
+
+
+        Service.initialize(Order, null);
+
+        Service.startPaymentTransaction(this, true, true,
+                new PaytmPaymentTransactionCallback() {
+
+                    @Override
+                    public void someUIErrorOccurred(String inErrorMessage) {
+                        // Some UI Error Occurred in Payment Gateway Activity.
+                        // // This may be due to initialization of views in
+                        // Payment Gateway Activity or may be due to //
+                        // initialization of webview. // Error Message details
+                        // the error occurred.
+                    }
+
+                    @Override
+                    public void onTransactionResponse(Bundle inResponse) {
+                        Log.d("LOG", "Payment Transaction : " + inResponse);
+                        Toast.makeText(getApplicationContext(), "Payment Transaction response "+inResponse.toString(), Toast.LENGTH_LONG).show();
+                    }
+
+                    @Override
+                    public void networkNotAvailable() {
+
+                    }
+
+                    @Override
+                    public void clientAuthenticationFailed(String inErrorMessage) {
+
+                    }
+
+                    @Override
+                    public void onErrorLoadingWebPage(int iniErrorCode,
+                                                      String inErrorMessage, String inFailingUrl) {
+
+                    }
+
+                    // had to be added: NOTE
+                    @Override
+                    public void onBackPressedCancelTransaction() {
+                        // TODO Auto-generated method stub
+                    }
+
+                    @Override
+                    public void onTransactionCancel(String inErrorMessage, Bundle inResponse) {
+                        Log.d("LOG", "Payment Transaction Failed " + inErrorMessage);
+                        Toast.makeText(getBaseContext(), "Payment Transaction Failed ", Toast.LENGTH_LONG).show();
+                    }
+
+                });
+    }
+}
